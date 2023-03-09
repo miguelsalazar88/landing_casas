a24eda8d5ba56b6fdbc4799cf606ed6a3f8f64d2
Ubuntu <ubuntu@ip-172-31-86-45.ec2.internal>
2023-03-09 19:35:44 +0000

    final commit

--- test_scraper.py
+++ test_scraper.py
@@ -1,7 +1,19 @@
-from scraper import lambda_handler, get_bucket, get_url
+from scraper import lambda_handler
 
 
 def test_lambda_handler1():
     response1 = lambda_handler()
     assert response1['statusCode'] == 200
     assert 'Contenido HTML guardado como' in response1['body']
+
+
+def test_lambda_handler2():
+    response2 = lambda_handler()
+    assert response2['statusCode'] == 200
+    assert 'Contenido HTML guardado como' in response2['body']
+
+
+def test_lambda_handler3():
+    response3 = lambda_handler()
+    assert response3['statusCode'] == 200
+    assert 'Contenido HTML guardado como' in response3['body']
